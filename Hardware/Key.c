34e348b13b32cc57c32badd2ab7db7d4b2087f8f
Armstrong <chengyouwei905@gmail.com>
2025-08-26 18:36:40 +0800

    key in stm32 cannot use

--- Hardware/Key.c
+++ Hardware/Key.c
@@ -1,34 +1,54 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
 
+uint8_t keynum = 0;
+
 void Key_Init(void)
 {
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
 	
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_11;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
 }
-
+/// @brief use in main while. if press, it will return de keynum for once.
+/// @param  void
+/// @return the key number you have press.
 uint8_t Key_GetNum(void)
 {
-	uint8_t KeyNum = 0;
-	if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 0)
+    uint8_t temp;
+    temp = keynum;
+    keynum = 0;
+    return temp;
+}
+
+uint8_t Key_GetState(void)
+{
+	if(GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_2) == 0)
 	{
-		Delay_ms(20);
-		while (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 0);
-		Delay_ms(20);
-		KeyNum = 1;
-	}
-	if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_11) == 0)
+        return 1;
+    }
+	else
+        return 0;
+}
+/// @brief use in timerhandler to turn on the key function.
+/// @param  void
+void Key_Tick(void)
+{
+    static uint8_t count;
+    static uint8_t nowstate, laststate;
+
+    count++;
+	if(count >= 20)
 	{
-		Delay_ms(20);
-		while (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_11) == 0);
-		Delay_ms(20);
-		KeyNum = 2;
-	}
-	
-	return KeyNum;
+        count = 0;
+        laststate = nowstate;
+        nowstate  = Key_GetState();
+		if(nowstate == 0 && laststate == 1)
+		{
+            keynum = nowstate;
+        }
+    }
 }
--- User/main.c
+++ User/main.c
@@ -3,6 +3,8 @@
 #include "OLED.h"
 #include "Serial.h"
 #include "LED.h"
+#include "Timer2.h"
+#include "Key.h"
 
 uint16_t test = 520;
 
@@ -13,10 +15,21 @@ int main(void)
     OLED_Printf(0, 0, OLED_8X16, "Hello, %3d", test);
     OLED_Update();
     LED_Init();
+    Timer2_Init();
+    Key_Init();
 
     while (1) {
-        Serial_Printf("Hello,world!");
-        LED_Turn();
-        Delay_ms(100);
+		if(Key_GetNum() == 1)
+		{
+			LED_Turn();
+		}
+    }
+}
+
+void TIM2_IRQHandler(void)
+{
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET) {
+        Key_Tick();
+        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
     }
 }
